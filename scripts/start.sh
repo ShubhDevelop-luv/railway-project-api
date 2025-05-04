50397ab5a6c9cad06456d237e442b16837bb529f
ShubhDevelop-luv <111449647+ShubhDevelop-luv@users.noreply.github.com>
2025-05-04 20:25:09 +0530

    add startup.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # Start FastAPI & Celery (Linux/Mac)
+#!/bin/bash
+echo "Starting FastAPI server..."
+
+# Activate virtual environment (optional)
+source env/bin/activate
+
+# Run Uvicorn server on Azure App Service
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload --workers 1 --timeout-keep-alive 60 --timeout-request 60 &
+# Run Celery worker with Redis as the broker
+celery -A main.celery worker --loglevel=info --concurrency=1 &  
